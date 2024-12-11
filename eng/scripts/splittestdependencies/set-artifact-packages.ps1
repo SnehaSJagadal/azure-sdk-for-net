ca3d32a601fb643df65c37a13ddde92bd8c75620
Scott Beddall <scbedd@microsoft.com>
2024-12-11 11:35:39 -0800

    ok, we should be able to build now

--- eng/scripts/splittestdependencies/set-artifact-packages.ps1
+++ eng/scripts/splittestdependencies/set-artifact-packages.ps1
@@ -6,6 +6,8 @@ param (
 
 . $PSScriptRoot/generate-dependency-functions.ps1
 
+$RepoRoot = Resolve-Path (Join-Path "$PSScriptRoot" ".." ".." "..")
+
 # set changed services given the set of changed packages, this will mean that
 # ChangedServices will be appropriate for the batched set of packages if that is indeed how
 # we set the targeted artifacts
@@ -24,11 +26,16 @@ $changedProjects = $packageProperties | Where-Object { $packageSet -contains $_.
 $projectsForGeneration = ($changedProjects | ForEach-Object { "`$(RepoRoot)$_" } | Sort-Object)
 $projectGroups = @()
 $projectGroups += ,$projectsForGeneration
+
+# todo: refactor write-test-dependency-group to take in a list of project files only and generate a single project file
 $outputFiles = Write-Test-Dependency-Group-To-Files -ProjectFileConfigName "packages" -ProjectGroups $projectGroups -MatrixOutputFolder $OutputPath
 
 # debug, will remove
 Get-ChildItem -Recurse $OutputPath | ForEach-Object { Write-Host "Dumping $($_.FullName)"; Get-Content -Raw -Path $_.FullName | Write-Host }
 
-Write-Host "##vso[task.setvariable variable=ProjectListOverrideFile;]$OutputPath/$($outputFiles[0])"
+# the projectlistoverride file must be provided as a relative path
+$relativeOutputPath = [System.IO.Path]::GetRelativePath($RepoRoot, "$OutputPath/$outputFiles[0]")
+
+Write-Host "##vso[task.setvariable variable=ProjectListOverrideFile;]$relativeOutputPath"
 Write-Host "##vso[task.setvariable variable=ChangedServices;]$changedServices"
 Write-Host "This run is targeting: $ProjectNames in [$changedServices]"
